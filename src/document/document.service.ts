cdadf61f76cf4b3a4ef6e2fa9cb27f4825efe645
arun-acharya-uk <arun.a@pacewisdom.com>
2025-05-13 20:05:37 +0530

    test

--- src/document/document.service.ts
+++ src/document/document.service.ts
@@ -423,7 +423,7 @@ async downloadImageToServer(imageUrl: string): Promise<string> {
 }
 async getVerifiableCredential(credentialId: string): Promise<any> {
     try {
-       const credential = await this.credentialsService.getCredentialsByCredentialId(credentialId);
+       const credential = await this.credentialsService.getCredentialsByCredentialForOther(credentialId);
        return credential;
     } catch (error) {
         if (error instanceof HttpException) throw error;    
