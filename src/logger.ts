72eee456df3b84919572096c97470d6a459d3b25
arun-acharya-uk <arun.a@pacewisdom.com>
2025-05-14 21:50:33 +0530

    added try catch

--- src/logger.ts
+++ src/logger.ts
@@ -2,24 +2,32 @@
 import * as winston from 'winston';
 import 'winston-daily-rotate-file';
 import * as path from 'path';
+const logDir = path.resolve(__dirname, '..', 'logs');
+export let logger: winston.Logger;
 
-const logDir = 'logs';
-
-const transport = new winston.transports.DailyRotateFile({
-  filename: path.join(logDir, 'application-%DATE%.log'),
-  datePattern: 'YYYY-MM-DD',
-  zippedArchive: false,
-  maxSize: '20m',
-  maxFiles: '30d', // Keep logs for 30 days
-});
-
-export const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(
-      ({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}] ${message}`,
-    ),
-  ),
-  transports: [transport],
-});
+try {
+    const transport = new winston.transports.DailyRotateFile({
+      filename: path.join(logDir, 'application-%DATE%.log'),
+      datePattern: 'YYYY-MM-DD',
+      maxFiles: '30d',
+      maxSize: '20m',
+      zippedArchive: false,
+    });
+  
+    logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(
+          ({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}] ${message}`
+        )
+      ),
+      transports: [transport],
+    });
+  
+    logger.info('Winston logger initialized successfully');
+  
+  } catch (err) {
+    console.error('Error setting up logger:', err);
+  }
+    
