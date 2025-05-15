7f8dd0c07636f4a1288a968f23223a4ae90bc620
arun-acharya-uk <arun.a@pacewisdom.com>
2025-05-15 14:27:14 +0530

    env changes

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,9 @@
+// src/config/env.ts
+import * as path from 'path';
 import * as dotenv from 'dotenv';
 
-// Load .env once when this file is imported
-dotenv.config();
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env'), // Adjust path to point to actual .env file
+});
 
 export const env = process.env;
