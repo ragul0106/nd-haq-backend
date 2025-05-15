b632db1dcb5a6d4627711c13822356ca6b79bf4a
arun-acharya-uk <arun.a@pacewisdom.com>
2025-05-15 14:10:34 +0530

    added log

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.use('/images', express.static(join(__dirname, '..', 'public', 'images')));
   app.use(new LoggerMiddleware().use);
-  console.log(process.env.PORT,"process.env.PORT");
+  console.log(process.env,"process.env.PORT");
   
   await app.listen(process.env.PORT ?? 3000);
 }
